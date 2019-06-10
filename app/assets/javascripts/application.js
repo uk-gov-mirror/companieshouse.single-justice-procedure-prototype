d0cd0a30916b670268e34b14e6d3517dc131ab59
Michael Gearon <mgearon@companieshouse.gov.uk>
2019-06-10 10:25:17 +0100

    Fixed the active btn on print

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -211,9 +211,9 @@ function dismiss () {
 
 // Parse the URL parameter
 function getParameterByName (name, url) {
-	    if (!url) url = window.location.href
-	    name = name.replace(/[\[\]]/g, '\\$&')
-	    var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
+  if (!url) url = window.location.href
+  name = name.replace(/[\[\]]/g, '\\$&')
+  var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
 	        results = regex.exec(url)
 	    if (!results) return null
 	    if (!results[2]) return ''
